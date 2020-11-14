5798a612b1650e3cf3c43d18d5ab4872206b1414
John Afolabi <johnafolabi.da@gmail.com>
2020-11-14 10:28:59 +0100

    Design navigation, repo filters

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,55 @@
-const query = `{
-    viewer {
-      repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}) {
-        edges {
-          node {
-            id
-            description
-            name
-            url
-            pushedAt
-            stargazerCount
-            forks {
-              totalCount
-            }
-            languages(orderBy: {field: SIZE, direction: DESC}, first: 1) {
-              nodes {
-                color
-                name
-              }
-            }
-          }
-        }
-      }
-      avatarUrl
-      bio
-      company
-      name
-      login
-      followers {
-        totalCount
-      }
-      following {
-        totalCount
-      }
-      starredRepositories {
-        totalCount
-      }
-      websiteUrl
-      location
-      email
-    }
-  }`;
+// const query = `{
+//     viewer {
+//       repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}) {
+//         edges {
+//           node {
+//             id
+//             description
+//             name
+//             url
+//             pushedAt
+//             stargazerCount
+//             forks {
+//               totalCount
+//             }
+//             languages(orderBy: {field: SIZE, direction: DESC}, first: 1) {
+//               nodes {
+//                 color
+//                 name
+//               }
+//             }
+//           }
+//         }
+//       }
+//       avatarUrl
+//       bio
+//       company
+//       name
+//       login
+//       followers {
+//         totalCount
+//       }
+//       following {
+//         totalCount
+//       }
+//       starredRepositories {
+//         totalCount
+//       }
+//       websiteUrl
+//       location
+//       email
+//     }
+//   }`;
 
-axios({
-	url: "https://api.github.com/graphql",
-	method: "post",
-	headers: {
-		Authorization: `bearer 7b092251e275873b0d76108e4827af2e1c46bff8`,
-	},
-	data: {
-		query,
-	},
-}).then((res) => {
-	console.log(res.data);
-});
+// axios({
+// 	url: "https://api.github.com/graphql",
+// 	method: "post",
+// 	headers: {
+// 		Authorization: `bearer 7b092251e275873b0d76108e4827af2e1c46bff8`,
+// 	},
+// 	data: {
+// 		query,
+// 	},
+// }).then((res) => {
+// 	console.log(res.data);
+// });
